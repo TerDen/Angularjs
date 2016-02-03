55a2401f85f5606c9ccc0a6cd13e143f2148c994
TerDen <Tereshkoda@gmail.com>
2016-02-03 17:19:56 +0300

    1) prepared html file: wrote the main tags, links 2) made files .js with controller description, function and variables 3) continued working with html: add necessary angularjs's tags 4) tested finished app

--- MainController.js
+++ MainController.js
@@ -1,3 +1,8 @@
-/**
- * Created by TerDen on 03.02.2016.
- */
+app.controller('MainController', ['$scope', function ($scope) {
+        $scope.summOfTwo = function(){
+        return document.getElementById("number1").value*1 + document.getElementById("number2").value*1;
+
+        // also can make such
+        //return $scope.number1 + $scope.number2
+        };
+}]);
